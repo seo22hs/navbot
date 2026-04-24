d99478eeff6d6e3e76ef3b0e4a76c671d9a2c262
seo22hs <lsc584727@gmail.com>
2026-04-23 18:12:00 -0700

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/seohee/navbot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/seohee/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/seohee/catkin_ws/devel;/opt/ros/noetic' != '' else []
